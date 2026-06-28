f948da3c89340a9ffab8924755a6e14b53042e34
SAJNUS <sajnussaharearhojayfa@gmail.com>
2026-06-28 08:55:09 +0600

    feat: add scroll-to-top navigation

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/shared/Navbar/Navbar";
 import Footer from "../components/shared/Footer/Footer";
+import ScrollToTop from "../components/shared/ScrollToTop";
 
 const MainLayout = () => {
   return (
     <div className="min-h-screen flex flex-col font-inter bg-base-100">
+      <ScrollToTop />
       <Navbar />
       <main className="flex-grow">
         <Outlet />
